18a21f1c83569a2dbdb19e50decec1c00ce2d67a
ariframaps <ariframa@student.ub.ac.id>
2024-10-22 14:28:21 +0700

    login page

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,17 @@ module.exports.register = async (req, res, next) => {
       return res.json({ status: false, msg: "Failed to add user" });
     });
 };
+
+module.exports.login = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    if (isValid) {
+      return res.json({ status: true, user: req.body.username });
+    } else {
+      return res.json({ status: false, msg: "Invalid username or password" });
+    }
+  } else {
+    return res.json({ status: false, msg: "Invalid username or password" });
+  }
+};
